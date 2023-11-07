d70330914150bdd5d96b0af61b19b1e22205a13d
simone-staccone <simone.staccone@virgilio.it>
2023-11-07 23:49:25 +0100

    Still some problems in local computing for integral trapezoidal method

--- integral_calculus/headers.h
+++ integral_calculus/headers.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 #include <mpi.h>
 
-float compute_definite_integral(float a, float b, unsigned long int iteration_number);
+float f(float x){
+    return x;
+}
--- integral_calculus/main.c
+++ integral_calculus/main.c
@@ -1,7 +1,10 @@
 #include "headers.h"
 
-float f(float x); //TODO Implement f function
-float Trap(float local_a, float local_b, int local_n, float h); //TODO Implement trap function
+
+/*****************************************************************
+* This is a program that wants to approximately compute integral * 
+* using trapezoidal method using inter-processes communication   *
+*****************************************************************/
 
 int main(int argc, char* argv[]) {
     int p, my_rank;
@@ -16,46 +19,71 @@ int main(int argc, char* argv[]) {
     // Setup MPI communicator
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    MPI_Comm_size(MPI_COMM_WORLD, &p); //Get number of processes
 
     //Initial communication
     tag = 0;
     if (my_rank == 0) {
-        printf("Enter a, b and n\n");
+        printf("My rank is: %d, please enter a, b and n\n",my_rank);
         scanf("%f %f %lu",&a,&b,&n);
     }
     if (my_rank == 0) {
         for (i=1; i<p; i++) {
             MPI_Send(&a,1,MPI_FLOAT,i,tag,MPI_COMM_WORLD);
             MPI_Send(&b,1,MPI_FLOAT,i,tag,MPI_COMM_WORLD);
-            MPI_Send(&n,1,MPI_INT,i,tag,MPI_COMM_WORLD);
+            MPI_Send(&n,1,MPI_UNSIGNED_LONG,i,tag,MPI_COMM_WORLD);
         }
     } else {
         MPI_Recv(&a,1,MPI_FLOAT,0,tag,MPI_COMM_WORLD,&status);
         MPI_Recv(&b,1,MPI_FLOAT,0,tag,MPI_COMM_WORLD,&status);
-        MPI_Recv(&n,1,MPI_INT,0,tag,MPI_COMM_WORLD,&status);
-
-        float local_a, local_b, local_int;
-        int local_n,
-        h = (b-a)/n;
-        local_n = n/p;
-        // Start of local integration
-        local_a = a+my_rank*local_n*h;
-        local_b = local_a+local_n*h;
-        local_int = Trap(local_a,local_b,local_n,h);
-
-        float temp;
-        tag = 1;
-        if (my_rank == 0) {
-            integral = local_int;
-            for (i=1; i<p; i++){
-                MPI_Recv(&temp,1,MPI_FLOAT,i,tag,MPI_COMM_WORLD,&status);
-                integral = integral + temp;
-            }
-            printf("With n=%lu trapezoids we estimate integral",n);
-            printf(" from %f to %f: %Lf\n",a,b,integral);
-        } else {
-            MPI_Send(&local_int,1,MPI_FLOAT,0,tag,MPI_COMM_WORLD);
-        }
+        MPI_Recv(&n,1,MPI_UNSIGNED_LONG,0,tag,MPI_COMM_WORLD,&status);
+        printf("[INFO] My rank is: %d, number arrived are: %f %f %lu\n",my_rank,a,b,n);
     }
+
+    // Start local computation
+    float local_a, local_b, local_int;
+    unsigned long int local_n;
+    unsigned long int r;
+
+    // Divide computation fairly between processes
+    r = n % p;
+    if (my_rank < r)
+    {
+        local_n = n / p + 1;
+        local_a = a + my_rank * local_n * h;
+        printf("[DEBUG] My rank is: %d, local values computed are: %f %f %lu\n",my_rank,local_a,local_b,local_n);
+    }
+    else
+    {
+        local_n = n / p + 1;
+        local_a = a + r * local_n * h;
+        local_n = n / p;
+        local_a = local_a + (my_rank - r) * local_n * h;
+    }
+    local_b = local_a + local_n * h;
+
+    h = (b - a) / n;
+    printf("[INFO] My rank is: %d, local values computed are: %f %f %lu\n",my_rank,local_a,local_b,local_n);
+
+
+
+    // compute_local_integral(local_a,local_b,local_n,h);
+    float temp;
+    tag = 1;
+    if (my_rank == 0)
+    {
+        integral = local_int;
+        for (i = 1; i < p; i++)
+        {
+            MPI_Recv(&temp, 1, MPI_FLOAT, i, tag, MPI_COMM_WORLD, &status);
+            integral = integral + temp;
+        }
+        printf("With n=%lu trapezoids we estimate integral", n);
+        printf(" from %f to %f: %Lf\n", a, b, integral);
+    }
+    else
+    {
+        MPI_Send(&local_int, 1, MPI_FLOAT, 0, tag, MPI_COMM_WORLD);
+    }
+    MPI_Finalize();
 }
